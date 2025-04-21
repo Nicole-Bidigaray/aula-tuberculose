b02a607676ca54553b07e7acbded2c3e8bbfaf69
Nicole Christine Baptista Bidigaray <nicole.bidigaray.prestserv@petrobras.com.br>
2025-04-21 19:00:58 -0300

    Foi consertado erros de dependencias e warnings

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,33 @@
+// next.config.mjs
+// import { fileURLToPath } from 'url'
+import { createRequire } from 'module'
+const require = createRequire(import.meta.url)
+
+// const __filename = fileURLToPath(import.meta.url)
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // For Static Export
-  // output: 'export',
   trailingSlash: true,
   images: {
     unoptimized: true,
   },
+
+  // Se precisar de rewrites, defina aqui
+  async rewrites() {
+    return [
+      // { source: '/api/:path*', destination: 'https://api.externa/:path*' }
+    ]
+  },
+
+  // Gatilho do Webpack para forçar o pacote 'punycode' do npm
+  webpack(config) {
+    config.resolve = config.resolve || {}
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      punycode: require.resolve('punycode/')
+    }
+    return config
+  }
 }
 
 export default nextConfig
